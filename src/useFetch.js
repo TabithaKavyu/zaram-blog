7d0274db483b26a3dc1ffa0c9625979d59869bef
TabithaKavyu <tabithakavyu4@gmail.com>
2021-03-27 15:24:35 +0300

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,8 +7,9 @@ const useFetch = (url) => {
      const [error, setError] =useState(null)
     
      useEffect(() => {
+         const abortCont = new AbortController();
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok) {
                     throw Error('could not fetch the data for that resource');
@@ -21,13 +22,16 @@ const useFetch = (url) => {
              setError(null);
          })
          .catch(err => {
+             if (err.name === 'AbortError') {
+                 console.log('fetch aborted');
+             }
              setIsPending(false); 
              setError(err.message);
          })
             
         }, 1000);
 
-      
+         return () => abortCont.abort();
     }, [url]);
 
     
